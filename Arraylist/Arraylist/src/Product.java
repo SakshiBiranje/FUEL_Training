c4eb4177d9cb34aa48b8c6cceed98eccecc1c87f
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2026-02-04 18:31:23 +0530

    Create Product.java

---
+++ Arraylist/Arraylist/src/Product.java
@@ -0,0 +1,23 @@
+
+public class Product {
+    int id;
+    String name;
+    double price;
+    int quantity;
+
+    public Product(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    void display() {
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Total Value: " + (price * quantity));
+        System.out.println();
+    }
+}
